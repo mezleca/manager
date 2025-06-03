6946552abb9ac89a1921854deb90d5411d90f5b4
mezleca <mtksenpai@gmail.com>
2025-06-03 14:56:31 -0300

    hotfix for window not opening on nvidia + wayland?

--- src/Program.cs
+++ src/Program.cs
@@ -1,10 +1,24 @@
-﻿namespace Main
+﻿using System.Runtime.InteropServices;
+
+namespace Main
 {
     public class Manager
     {
+
+        [DllImport("libc.so.6", SetLastError = true)]
+        private static extern int setenv(string name, string value, int overwrite);
         [STAThread]
         static void Main(string[] args)
         {
+            // classic nvidia + wayland moment
+            if (Window.is_linux)
+            {
+                Console.WriteLine("enabling extra env variables");
+                _ = setenv("GDK_BACKEND", "x11", 1);
+                _ = setenv("WEBKIT_DISABLE_COMPOSITING_MODE", "1", 1);
+                _ = setenv("WEBKIT_DISABLE_DMABUF_RENDERER", "1", 1);
+            }
+
             Window.Initialize();
         }
     }

e180fb837f0cd36a169d06091eb6323b661ee428
mezleca <mtksenpai@gmail.com>
2025-06-19 00:00:05 -0300

    check if webkit2gtk is available before opening

--- src/Program.cs
+++ src/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System.Runtime.InteropServices;
-using App;
+using System;
 namespace Main;
 
 public class Entry
@@ -10,15 +10,24 @@ public class Entry
     [STAThread]
     static void Main(string[] args)
     {
-        // classic nvidia moment
         if (Window.is_linux) {
             
             var LIBVA_DRIVER_NAME = Environment.GetEnvironmentVariable("LIBVA_DRIVER_NAME");
       
-            // fix webview not loading due to some stupid ass nvidia bug
             if (LIBVA_DRIVER_NAME == "nvidia") {
+                // prevent webview not loading due to some stupid ass nvidia bug
                 _ = setenv("WEBKIT_DISABLE_COMPOSITING_MODE", "1", 1);
                 _ = setenv("WEBKIT_DMABUF_RENDERER_DISABLE_GBM", "1", 1);
+            }
+
+            // @TODO: chjeck if whatever webview used on windows is present
+            // also use showmessagebox or something
+
+            // @NOTE: may vary depending on the distro? idk
+            if (!File.Exists("/usr/lib/libwebkit2gtk-4.0.so") && !Directory.Exists("/usr/lib/webkit2gtk-4.0")) {
+                var command = "notify-send --app-name='manager' -h 'string:sound-name:message-new-instant' 'missing libwebkit2gtk' 'please install webkit2gtk on your distro vro'";
+                Utils.Exec(command);
+                Environment.Exit(1);
             }
         }
 
